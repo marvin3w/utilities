0a035c1ee7507d782d86253d1c78348ce2e4beac
Marcus Oliveira <marcus.oliveira@base.digital>
2024-09-29 04:27:18 -0300

    chore: config webpack process scss to css

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const glob = require('glob');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: () => {
+    const entries = {};
+    const scssFiles = glob.sync('./**/*.scss');
+    
+    scssFiles.forEach(file => {
+      const name = file.replace(/^\.\//, '').replace(/\.scss$/, '');
+      entries[name] = './' + file;
+    });
+    
+    return entries;
+  },
+  output: {
+    path: path.resolve(__dirname),
+    filename: '[name].js', // Isso não será usado, mas é necessário para o Webpack
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: (pathData) => {
+        return pathData.chunk.name.includes('/') 
+          ? '[name].css' 
+          : '[name]/[name].css';
+      }
+    })
+  ],
+  watch: true,
+  mode: 'development',
+  resolve: {
+    modules: [path.resolve(__dirname), 'node_modules'],
+  },
+};
