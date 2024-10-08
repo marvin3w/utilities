2ec7bac574d17c0ebafbdfe5b9e24ed21aab00cf
Marcus Oliveira <marcus.oliveira@base.digital>
2024-10-08 02:39:51 -0300

    adjustements

--- btn-toggle/main.js
+++ btn-toggle/main.js
@@ -1,15 +1,12 @@
-// Função para detectar o tema padrão do sistema operacional
 function getSystemDefaultTheme() {
     return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
 }
 
-// Função para aplicar o tema
 function applyTheme(theme) {
     document.body.classList.toggle('dark-mode', theme === 'dark');
     document.getElementById('toggle').checked = theme === 'dark';
 }
 
-// Carregar o tema ao iniciar
 (function () {
     let theme = localStorage.getItem('theme');
 
@@ -21,7 +18,6 @@ function applyTheme(theme) {
     applyTheme(theme);
 })();
 
-// Alterar o tema ao clicar no toggle
 document.getElementById('toggle').addEventListener('change', function () {
     let theme = this.checked ? 'dark' : 'light';
     localStorage.setItem('theme', theme);
