74291e529ab9db54c864fc95f1d049bd282bbf29
Marcus Oliveira <marcus.oliveira@base.digital>
2024-11-06 00:11:22 -0300

    fix: general fixes

--- periodic-table/main.js
+++ periodic-table/main.js
@@ -1,5 +1,13 @@
 let openCard = null;
 
+const debounce = (fn, ms) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
+
 document.querySelectorAll('.element-card').forEach(card => {
   
   card.addEventListener('click', () => {
